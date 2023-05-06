b473bc4a522e0b4f3b0ef63431ed70c122427dce
RoisinMitchell <mitchell.roisin@gmail.com>
2023-05-06 16:07:00 +0100

    Created method

--- multRecursive.java
+++ multRecursive.java
@@ -1,2 +1,9 @@
 public class multRecursive {
+    public static int mult(int x, int y){
+        if(y != 0){
+            return x + mult(x, (y-1));
+        }else{
+            return 0;
+        }
+    }
 }
